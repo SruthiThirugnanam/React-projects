3f9660f7ed4fd54d11fc0d9bab998ef79d1eda3c
SruthiThirugnanam <ponsruthi2020@gmail.com>
2025-09-02 02:34:58 +0530

    Update vite.config.js

--- Project-1/vite.config.js
+++ Project-1/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/React-projects/', // 👈 This tells Vite where your app is hosted
+  base: '/React-projects/Project-1/', // 👈 This tells Vite where your app is hosted
 })
