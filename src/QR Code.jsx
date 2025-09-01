e854ae621b52948a67dfcf262c889bf044310cc8
SruthiThirugnanam <ponsruthi2020@gmail.com>
2025-09-02 01:18:17 +0530

    Update QR Code.jsx

--- src/QR Code.jsx
+++ src/QR Code.jsx
@@ -76,7 +76,7 @@ export const QRCode = () => {
         className="github-button"
         style={{
           display: "inline-block",
-          marginTop: "25px",
+          marginTop: "40px",
           padding: "10px 20px",
           backgroundColor: "#24292e",
           color: "#ffffff",
