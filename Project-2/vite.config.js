67942592e21a8b2f09736f45f4088fc5dae9fe0c
SruthiThirugnanam <ponsruthi2020@gmail.com>
2025-09-02 03:18:54 +0530

    Update vite.config.js

--- Project-2/vite.config.js
+++ Project-2/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/React-projects/Project-2/', // ✅ This is required for GitHub Pages
+  base: './',  // ⚠️ Important: use relative base path
 })
